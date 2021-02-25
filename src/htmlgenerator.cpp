ab1bc76905dbae2596ad57bf316f85abd286c7fa
erdincyilmaz <ahmeterdincyilmaz@gmail.com>
2021-02-25 21:18:31 +0300

    html generator

---
+++ src/htmlgenerator.cpp
@@ -0,0 +1,47 @@
+#include "htmlgenerator.h"
+
+#include <QDebug>
+#include <QFile>
+#include <QRegularExpression>
+#include <QString>
+
+int main() {
+  QFile templateHtmlFile(INDEX_TEMPLATE_HTML);
+  if (!templateHtmlFile.exists()) {
+    qDebug() << "NO FILE " << INDEX_TEMPLATE_HTML;
+    exit(-1);
+  }
+
+  QString htmlTemplate = "<!-- THIS FILE IS GENERATED, DO NOT EDIT! -->\n";
+  if (templateHtmlFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+    htmlTemplate = htmlTemplate + QString::fromUtf8(templateHtmlFile.readAll());
+    templateHtmlFile.close();
+  } else {
+    qDebug() << "Cannot Open File " << INDEX_TEMPLATE_HTML;
+    exit(-1);
+  }
+
+  QString darkHtml = htmlTemplate;
+  darkHtml.replace("{markdown_theme}", "node-dark");
+  darkHtml.replace("{scroll_bar_theme}", "dark_scroll_bar");
+  darkHtml.replace("{highlight_theme}", "dark");
+
+  QString lightHtml = htmlTemplate;
+  lightHtml.replace("{markdown_theme}", "github");
+  lightHtml.replace("{scroll_bar_theme}", "light_scroll_bar");
+  lightHtml.replace("{highlight_theme}", "github");
+
+  QFile darkHtmlFile(INDEX_DARK_HTML);
+  if (darkHtmlFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+    QTextStream stream(&darkHtmlFile);
+    stream << darkHtml;
+    darkHtmlFile.close();
+  }
+
+  QFile lightHtmlFile(INDEX_LIGHT_HTML);
+  if (lightHtmlFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+    QTextStream stream(&lightHtmlFile);
+    stream << lightHtml;
+    lightHtmlFile.close();
+  }
+}
